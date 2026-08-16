27465096ce35e128368b1b919ca13dcfa6777a6f
Akshar Thaker <aksharthaker2@gmail.com>
2026-08-16 20:36:31 +0530

    Day 4: first untuned SVD baseline, RMSE 0.89 / MAE 0.70, in line with published benchmarks

---
+++ src/svd_baseline.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from surprise import Dataset, Reader, SVD
+from surprise import accuracy
+from src.data_loader import load_ratings, time_based_split
+
+def to_surprise_dataset(df, rating_scale=(1, 5)):
+    """
+    Convert a pandas DataFrame of (user_id, movie_id, rating) into a
+    surprise Dataset object.
+    """
+    reader = Reader(rating_scale=rating_scale)
+    data = Dataset.load_from_df(df[["user_id", "movie_id", "rating"]], reader)
+    return data
+
+if __name__ == "__main__":
+    ratings = load_ratings()
+    train_df, test_df = time_based_split(ratings, test_frac=0.2)
+
+    # Build surprise training set from our train split
+    train_data = to_surprise_dataset(train_df)
+    trainset = train_data.build_full_trainset()
+
+    # Untuned SVD model - default hyperparameters, just to prove it runs
+    model = SVD()
+    model.fit(trainset)
+
+    # Build test set in surprise's expected format: list of (user, item, true_rating) tuples
+    testset = list(test_df[["user_id", "movie_id", "rating"]].itertuples(index=False, name=None))
+
+    predictions = model.test(testset)
+
+    rmse = accuracy.rmse(predictions)
+    mae = accuracy.mae(predictions)
